8e36ae4615755db16a750596c772a00196fe84d7
wilfried <wilfried.odorico@gmail.com>
2024-10-04 16:12:53 +0200

    add method for get best movies with imdb score greather then 9

--- scripts/api_movies_services.js
+++ scripts/api_movies_services.js
@@ -24,6 +24,15 @@ class ApiMoviesServices {
         }
     }
 
+    async getBestMoviesWithImdbScoreAbove9() {
+        return this.getMovies({imdb_score_min: '9'})
+            .then(data => data)
+            .catch(error => {
+                console.error(error);
+                throw error;
+            });
+    }
+
     async getBestMovie() {
         return this.getMovies({sort_by: '-imdb_score'})
             .then(movies => movies.results[0])
@@ -47,7 +56,7 @@ class ApiMoviesServices {
 
 const serviceApi = new ApiMoviesServices('http://localhost:8000/api/v1/titles/');
 
-let bestMovies = serviceApi.getBestMoviesWithImdbAbove9();
+let bestMovies = serviceApi.getBestMoviesWithImdbScoreAbove9();
 bestMovies.then(data => {
     console.log("bestMovies", data);
 });
@@ -56,3 +65,44 @@ let movie = serviceApi.getMovieById(12492650);
 movie.then(data => {
     console.log("movie", data);
 });
+
+
+// class ServiceApiMovies {
+//     constructor(baseUrl) {
+//         this.baseUrl = baseUrl;
+//     }
+
+//     async getTheBestMovie() {
+//         try {
+//             const response = await fetch(this.baseUrl+"?sort_by=-imdb_score");
+//             const data = await response.json();
+//             return data.results[0];
+//         } catch (error) {
+//             console.error(error);
+//             throw error;
+//         }
+//     }
+
+//     async getBestMoviesByMinScore(minScore) {
+//         let url = this.baseUrl+`?imdb_score_min=${minScore}`;
+//         let nextPage = url;
+//         let movies = [];
+//         while(nextPage){
+//             const response = await fetch(nextPage);
+//             const data = await response.json();
+//             movies.push(...data.results);
+//             nextPage = data.next;
+//         }
+//         return movies;
+//     }
+// }
+
+// const serviceApiMovies = new ServiceApiMovies('http://localhost:8000/api/v1/titles/');
+
+// let data = serviceApiMovies.getTheBestMovie();
+// data.then(data => {
+//     console.log(data);
+// });
+
+// let data2 = serviceApiMovies.getBestMoviesByMinScore(9);
+// data2.then(data => console.log(data));
