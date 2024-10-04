4b62a462ce37dae1a9468d9c8270d1d7bf9da82e
wilfried <wilfried.odorico@gmail.com>
2024-10-04 15:59:00 +0200

    created file api movies services and created class ApiMoviesServices

---
+++ scripts/api_movies_services.js
@@ -0,0 +1,42 @@
+class ApiMoviesServices {
+    constructor(baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    // Construire une URL avec les filtres
+    buildUrl(filters={}) {
+        const url = new URL(this.baseUrl);
+        Object.keys(filters).forEach(key => {
+            url.searchParams.append(key, filters[key]);
+        });
+        return url.toString();
+    }
+
+    async getMovies(filters={}) {
+        const url = this.buildUrl(filters);
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+    async getBestMovie() {
+        return this.getMovies({sort_by: '-imdb_score'})
+            .then(movies => movies.results[0])
+            .catch(error => {
+                console.error(error);
+                throw error;
+            });
+    }
+}
+
+const serviceApi = new ApiMoviesServices('http://localhost:8000/api/v1/titles/');
+
+let bestMovies = serviceApi.getBestMoviesWithImdbAbove9();
+bestMovies.then(data => {
+    console.log("data", data);
+});
