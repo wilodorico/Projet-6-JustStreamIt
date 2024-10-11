cb02d36d04b02cd1dd823c6484d8f7af7fd7df56
wilfried <wilfried.odorico@gmail.com>
2024-10-11 16:22:47 +0200

    refacto main js

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ const bestMovieDescription = document.getElementById('best-movie-description');
 
 const serviceApi = new ServiceApiMovies('http://localhost:8000/api/v1/');
 
+// Fonction pour ajouter un film à une section avec son template
 function addMovieToSection(movie, sectionId) {
     const template = document.getElementById("movie-template");
     const clone = template.content.cloneNode(true);
@@ -19,37 +20,45 @@ function addMovieToSection(movie, sectionId) {
 };
 
 
-serviceApi.getBestMovie()
-    .then(movie => {
-        bestMovieImage.src = movie.image_url;
-        bestMovieImage.alt = movie.title;
-        bestMovieTitle.innerHTML = movie.title;
-        bestMovieDescription.innerHTML = movie.description;
-    })
-    .catch(error => {
-        console.error("Erreur lors de la récupération du meilleur film :", error);
-    })
-
-serviceApi.getBestMoviesWithImdbAbove9()
-    .then(movies => {
-        movies.forEach(movie => {
-            addMovieToSection(movie, 'bestMovies');
+// Fonction pour récupérer et afficher le meilleur film
+function displayBestMovie() {
+    serviceApi.getBestMovie()
+        .then(movie => {
+            bestMovieImage.src = movie.image_url;
+            bestMovieImage.alt = movie.title;
+            bestMovieTitle.innerHTML = movie.title;
+            bestMovieDescription.innerHTML = movie.description;
+        })
+        .catch(error => {
+            console.error("Erreur lors de la récupération du meilleur film :", error);
         });
-    });
+}
 
-serviceApi.getBestMoviesByGenre("Thriller")
-    .then(movies => {
-        movies.forEach(movie => {
-            addMovieToSection(movie, 'bestMoviesThriller');
+// Fonction pour récupérer et afficher les films les mieux notés
+function displayBestMovies() {
+    serviceApi.getBestMoviesWithImdbAbove9()
+        .then(movies => {
+            movies.forEach(movie => {
+                addMovieToSection(movie, 'bestMovies');
+            });
+        })
+        .catch(error => {
+            console.error("Erreur lors de la récupération des meilleurs films :", error);
         });
-    });
+}
 
-serviceApi.getBestMoviesByGenre("Family")
-    .then(movies => {
-        movies.forEach(movie => {
-            addMovieToSection(movie, 'bestMoviesFamily');
+// Fonction pour récupérer et afficher les films par genre
+function displayMoviesByGenre(genre, sectionId) {
+    serviceApi.getBestMoviesByGenre(genre)
+        .then(movies => {
+            movies.forEach(movie => {
+                addMovieToSection(movie, sectionId);
+            });
+        })
+        .catch(error => {
+            console.error(`Erreur lors de la récupération des films ${genre} :`, error);
         });
-    });
+};
 
 // Pour voir le format des données
 let getDetailsMovieById = serviceApi.getDetailsMovieById(12492650);
@@ -72,4 +81,8 @@ bestMoviesByGenre.then(data => {
     console.log("getBestMoviesByGenre", data);
 });
 
+displayBestMovie();
+displayBestMovies();
+displayMoviesByGenre("Thriller", "bestMoviesThriller");
+displayMoviesByGenre("Family", "bestMoviesFamily");
 
