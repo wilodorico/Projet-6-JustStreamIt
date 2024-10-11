3ee389095b17ff47637367d3453474072757cdff
wilfried <wilfried.odorico@gmail.com>
2024-10-11 17:54:56 +0200

    Added select category and fill options with category and add evenListener for display movie by category selected

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,15 @@
 const bestMovieImage = document.getElementById('best-movie-image');
 const bestMovieTitle = document.getElementById('best-movie-title');
 const bestMovieDescription = document.getElementById('best-movie-description');
+const selectCategory = document.getElementById('category');
 
 const serviceApi = new ServiceApiMovies('http://localhost:8000/api/v1/');
+
+
+// Fonction pour vider une section
+function clearSection(sectionId) {
+    document.getElementById(sectionId).innerHTML = '';
+}
 
 // Fonction pour ajouter un film à une section avec son template
 function addMovieToSection(movie, sectionId) {
@@ -17,7 +24,7 @@ function addMovieToSection(movie, sectionId) {
     movieTitle.textContent = movie.title;
 
     document.getElementById(sectionId).appendChild(clone);
-};
+}
 
 
 // Fonction pour récupérer et afficher le meilleur film
@@ -49,6 +56,7 @@ function displayBestMovies() {
 
 // Fonction pour récupérer et afficher les films par genre
 function displayMoviesByGenre(genre, sectionId) {
+    clearSection(sectionId);
     serviceApi.getBestMoviesByGenre(genre)
         .then(movies => {
             movies.forEach(movie => {
@@ -59,6 +67,32 @@ function displayMoviesByGenre(genre, sectionId) {
             console.error(`Erreur lors de la récupération des films ${genre} :`, error);
         });
 };
+
+
+function fillSelectOptionCategory() {
+    serviceApi.getGenres()
+    .then(data => {
+        data.forEach(categorie => {
+            const option = document.createElement('option');
+            option.value = categorie.id;
+            option.text = categorie.name;
+            selectCategory.add(option, null);
+        })
+    });
+};
+fillSelectOptionCategory();
+
+// Fonction pour récupérer et afficher les films par catégorie
+// TODO : Voir certaines catégories ne sont pas affichées car bug ou problème de l'API
+const bestMoviesByCategoryChoice = document.getElementById('bestMoviesByCategoryChoice');
+displayMoviesByGenre("Action", "bestMoviesByCategoryChoice");
+
+selectCategory.addEventListener("change", (event) => {
+    const selectedCategory = event.target.options[event.target.selectedIndex].text;
+    displayMoviesByGenre(selectedCategory, "bestMoviesByCategoryChoice");
+    console.log('selectedCategory :>> ', selectedCategory);
+})
+
 
 // Pour voir le format des données
 let getDetailsMovieById = serviceApi.getDetailsMovieById(12492650);
@@ -80,6 +114,7 @@ let bestMoviesByGenre = serviceApi.getBestMoviesByGenre("Thriller");
 bestMoviesByGenre.then(data => {
     console.log("getBestMoviesByGenre", data);
 });
+
 
 displayBestMovie();
 displayBestMovies();
